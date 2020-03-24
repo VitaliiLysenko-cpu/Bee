60e1a0c75762dbba9af8237baa051000c8f56913
Vitalii <vit47903@gmail.com>
2020-03-24 20:59:25 +0200

    added QueenBee

--- app/src/main/java/com/example/beegame/MainActivity.java
+++ app/src/main/java/com/example/beegame/MainActivity.java
@@ -2,44 +2,56 @@ package com.example.beegame;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import static androidx.constraintlayout.widget.ConstraintSet.BOTTOM;
+import static androidx.constraintlayout.widget.ConstraintSet.END;
+import static androidx.constraintlayout.widget.ConstraintSet.LEFT;
+import static androidx.constraintlayout.widget.ConstraintSet.PARENT_ID;
+import static androidx.constraintlayout.widget.ConstraintSet.TOP;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    
     ConstraintLayout constraintLayout;
-    ConstraintSet set = new ConstraintSet();
-
-
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         constraintLayout = new ConstraintLayout(this);
         constraintLayout = findViewById(R.id.constraint_layout);
-
+        
+        ConstraintSet set = new ConstraintSet();
+        
+        ImageView imageViewQueenBee = new ImageView(this);
+        int queenBeeIdView = View.generateViewId();
+        imageViewQueenBee.setId(queenBeeIdView);
+        constraintLayout.addView(imageViewQueenBee);
+        imageViewQueenBee.setImageResource(R.drawable.queen_bee);
+        set.connect(PARENT_ID, LEFT, queenBeeIdView, LEFT, 8);
+        set.connect(PARENT_ID, TOP, queenBeeIdView, TOP, 8);
+        
+        int prewIdView = queenBeeIdView;
         for (int i = 1; i <= 5; i++) {
             ImageView imageViewWorkerBee = new ImageView(this);
-            imageViewWorkerBee.setId(imageViewWorkerBee. generateViewId ());
+            int idThisView = View.generateViewId();
+            imageViewWorkerBee.setId(idThisView);
             constraintLayout.addView(imageViewWorkerBee);
             imageViewWorkerBee.setImageResource(R.drawable.worker_bee);
-            set.clone(constraintLayout);
-            set.connect(imageViewWorkerBee.getId(),ConstraintSet.LEFT, constraintLayout.NO_ID,
-                    ConstraintSet.END);
-
-
+            set.connect(prewIdView, LEFT, idThisView, LEFT);
+            set.connect(prewIdView, BOTTOM, idThisView, TOP);
+            
+            prewIdView = idThisView;
+            set.applyTo(constraintLayout);
+            
+            
         }
-
-
-
     }
-
-  // private void changeConstraints(ConstraintSet set) {
-  //     set.connect(R.id.constraint_layout, ConstraintSet.LEFT,R.id.);
-  //     //set.connect(R.id.constraint_layout, ConstraintSet.BOTTOM, R.id.parent, ConstraintSet.TOP);
-  // }
 }
